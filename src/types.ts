853aabacad764fde54ec7c42d26d554a97a79bf5
Tampoka <goal.thanks@gmail.com>
2022-08-12 13:02:40 +0900

    Add interface.

--- src/types.ts
+++ src/types.ts
@@ -3,8 +3,8 @@ export type UniqueString = string
 export type EntityId = number | UniqueString
 export type DateIsoString = string
 
-export type Post = {
-    _id: EntityId
+export interface IPost {
+    readonly _id: EntityId
     date: DateIsoString
     category: Category
     title: string
@@ -18,10 +18,11 @@ export type Category = "科学・IT" | "社会" | "経済"
 
 export type Person = string
 export type RelativeTime = string
-export type Comment = {
-    id: EntityId
+
+export interface Comment {
+    readonly _id: EntityId
     author: Person
     content: string
     time: RelativeTime
-    post: EntityId
+    readonly post: EntityId
 }
