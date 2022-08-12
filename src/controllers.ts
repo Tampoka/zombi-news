714c0df52d05499dda6efa009a5b8549bf76d8e7
Tampoka <goal.thanks@gmail.com>
2022-08-12 23:26:44 +0900

    Fix createComments to return comments array.

--- src/controllers.ts
+++ src/controllers.ts
@@ -2,7 +2,7 @@ import {Request, Response} from 'express';
 
 const {getAllPosts, getPostById, getPostsByCategory} = require('./posts-repo')
 const {getAllCategories} = require('./categories-repo')
-const {getPostComments,addComment} = require('./comments-repo')
+const {getPostComments, addComment} = require('./comments-repo')
 
 //posts
 async function getPosts(_req: Request, res: Response) {
@@ -17,7 +17,7 @@ async function getPosts(_req: Request, res: Response) {
 async function getPost(req: Request, res: Response) {
     try {
         const post = await getPostById(req.params.id)
-        res.send({post})
+        res.send(post)
     } catch
         (error) {
         res.send(error)
@@ -38,7 +38,7 @@ async function getCategoryPosts(req: Request, res: Response) {
     try {
         const categoryValue = req.params.id
         const posts = await getPostsByCategory(categoryValue)
-        const categoryPosts=[...posts,...posts,...posts]
+        const categoryPosts = [...posts, ...posts, ...posts]
         res.send(categoryPosts)
     } catch (error) {
         res.send(error)
@@ -59,13 +59,15 @@ async function getComments(req: Request, res: Response) {
 async function createComment(req: Request, res: Response) {
     try {
         const postId = req.params.id
-        const{name,comment}=req.body
-         await addComment  (postId, name, comment)
-        res.sendStatus(201)
+        const {name, comment} = req.body
+        await addComment(postId, name, comment)
+        const comments = await getPostComments(postId)
+        res.send(comments)
     } catch (error) {
         res.send(error)
     }
 }
+
 module.exports = {
     getPosts,
     getPost,
